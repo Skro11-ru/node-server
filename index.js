cd6aa2b25df6b0776b5bea7ca8c68f33ab4d3330
Skro11 <bunetskydn@gmail.com>
2024-08-05 15:03:19 +0700

    c

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const notesRouter = require('./routes/notes');
 
 const app = express();
 app.use(express.json()); // Парсинг JSON
 app.use(express.urlencoded({ extended: true })); // Парсинг URL-encoded данных
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/notes', notesRouter);
 
 const PORT = process.env.PORT || 3000;
