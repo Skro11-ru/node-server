c2cef00d2e7c72ed2e9427dcfc5d2202288f0b73
Skro11 <bunetskydn@gmail.com>
2024-08-05 15:10:45 +0700

    c

--- db.js
+++ db.js
@@ -7,4 +7,17 @@ const pool = new Pool({
 	}
 });
 
+// Создание таблицы, если она не существует
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS notes (
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`;
+
+pool.query(createTableQuery)
+	.then(res => console.log('Table is successfully created'))
+	.catch(err => console.error('Error creating table', err.stack));
+
 module.exports = pool;
